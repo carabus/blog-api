8ccbbbca3a0d89d64cd7a5791583250bfc07c95f
Svetlana Raeva <svetlana.myth@gmail.com>
2018-04-15 18:18:32 -0700

    implemented CRUD for Blog Posts API

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const blogPostsRouter = require("./blogPostsRouter");
+
+// log the http layer
+app.use(morgan("common"));
+
+// when requests come into `/blogs`
+// we'll route them to the express
+// router instance we've imported.
+app.use("/blogPosts", blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
